001b8e27009118c76047700606be0c1d502d7619
danielsmetana7 <93688227+danielsmetana7@users.noreply.github.com>
2021-11-23 14:26:56 -0600

    Create Contextual_Search_App.py

---
+++ Contextual_Search_App.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: danielsmetana7@ MABA CLASS
+"""
+
+import streamlit as st
+import pandas as pd
+
+import plotly.express as px
+
+
+st.title("Daniel Smetana Chicago Contextual Search Engine")
