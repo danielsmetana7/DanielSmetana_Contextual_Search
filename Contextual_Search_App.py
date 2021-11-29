fc9cdff468b8e570dea22ac5d949e44899a734f8
danielsmetana7 <93688227+danielsmetana7@users.noreply.github.com>
2021-11-29 13:34:22 -0600

    Update Contextual_Search_App.py

--- Contextual_Search_App.py
+++ Contextual_Search_App.py
@@ -61,7 +61,6 @@ df_sentences = df_combined.set_index("all_review")
 
 df_sentences = df_sentences["hotelName"].to_dict()
 df_sentences_list = list(df_sentences.keys())
-len(df_sentences_list)
 
 #list(df_sentences.keys())[:5]
 
@@ -74,44 +73,44 @@ df_sentences_list = [str(d) for d in tqdm(df_sentences_list)]
 
 search_criteria = st.text_input("Please enter search criteria: ")
 
-print(search_criteria)
+str(search_criteria)
+
 
 # Corpus with example sentences
 corpus = df_sentences_list
 corpus_embeddings = embedder.encode(corpus,show_progress_bar=True)
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
-queries = ['Hotel not too far from Sears Tower',
-           'Hotel closest to Navy Pier'
-           ]
+queries = search_criteria
+
 paraphrases = util.paraphrase_mining(model, corpus)
-query_embeddings_p =  util.paraphrase_mining(model, queries, show_progress_bar=True)
+query_embeddings_p =  util.paraphrase_mining(model, search_criteria, show_progress_bar=True)
 
 
-query_embeddings = embedder.encode(queries,show_progress_bar=True)
+query_embeddings = embedder.encode(search_criteria,show_progress_bar=True)
 
 
 # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
 closest_n = 5
-print("\nTop 5 most similar sentences in corpus:")
-for query, query_embedding in zip(queries, query_embeddings):
+st.text("Top 5 most similar hotels:")
+for query, query_embedding in zip(search_criteria, query_embeddings):
     distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
 
     results = zip(range(len(distances)), distances)
     results = sorted(results, key=lambda x: x[1])
 
-    print("\n\n=========================================================")
-    print("==========================Query==============================")
-    print("===",query,"=====")
-    print("=========================================================")
+    st.text("\n\n=========================================================")
+    st.text("==========================Query==============================")
+    st.text("===",query,"=====")
+    st.text("=========================================================")
 
 
     for idx, distance in results[0:closest_n]:
-        print("Score:   ", "(Score: %.4f)" % (1-distance) , "\n" )
-        print("Paragraph:   ", corpus[idx].strip(), "\n" )
+        st.text("Score:   ", "(Score: %.4f)" % (1-distance) , "\n" )
+        st.text("Paragraph:   ", corpus[idx].strip(), "\n" )
         row_dict = df.loc[df['all_review']== corpus[idx]]
-        print("paper_id:  " , row_dict['hotelName'] , "\n")
+        st.text("paper_id:  " , row_dict['hotelName'] , "\n")
         # print("Title:  " , row_dict["title"][corpus[idx]] , "\n")
         # print("Abstract:  " , row_dict["abstract"][corpus[idx]] , "\n")
         # print("Abstract_Summary:  " , row_dict["abstract_summary"][corpus[idx]] , "\n")
-        print("-------------------------------------------")
+        st.text("-------------------------------------------")
